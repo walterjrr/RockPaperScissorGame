c44651a56f73e22e7733ac6594240de8a6a4d1ff
walterjrr <jrwalter731@gmail.com>
2023-11-17 21:03:10 -0300

    refactor all code

--- dev/script.js
+++ dev/script.js
@@ -1,159 +1,91 @@
 const randomText = document.getElementById("random");
-
 const rockbutton = document.getElementById("rock-button");
 const scissorbutton = document.getElementById("scissor-button");
 const paperbutton = document.getElementById("paper-button");
 const rules = document.getElementById("rules");
 const score = document.getElementById("Score");
-
 const imageadversary = document.getElementById("imageadversary");
 const yourimage = document.getElementById("yourimage");
-
 const loading = document.getElementById("spinner");
-
 const rulesimage = document.getElementById("rulesimage");
 
 
 let placarScore = 0
 
-function updateScoreboard() {
-    const placar = document.getElementById("placar");
+function updateScoreboard(resultado) {
 
-    placar.textContent = placarScore
+
+    const placar = document.getElementById("placar");
+    placar.textContent = placarScore;
+    showLoading();
+    setTimeout(hideloading, 1000);
+
+    if (resultado === "drawn") {
+        hideloading("drawn")
+    } else if (resultado === "winner") {
+        hideloading("winner")
+    } else if (resultado === "lost"){
+        hideloading("lost")
+    }
+
+console.log(resultado)
+}
+function showLoading() {
+    loading.style.display = "block"
+    imageadversary.style.display = "none"
+    score.innerHTML = "loading..."
+}
+function hideloading(resultado) {
+    setTimeout(() => {
+        loading.style.display = "none";
+        imageadversary.style.display = "block";
+
+        if (resultado === "drawn") {
+            score.innerHTML = "drawn";
+        } else if (resultado === "winner") {
+            score.innerHTML = "winner";
+        } else if (resultado === "lost") {
+            score.innerHTML = "lost";
+        }
+
+        console.log("1", resultado);
+    }, 1000);
 }
 
+function PlayGames(playerChoice) {
 
-rockbutton.addEventListener("click", function() {
-    function showLoading() {
-        loading.style.display = "block"
-        imageadversary.style.display = "none"
-    }
-    showLoading()
-    function hideloading() {
-        loading.style.display = "none"
-        imageadversary.style.display = "block"
-    }
     let randomValue = Math.floor(Math.random() * 3);
-    setTimeout(hideloading, 1000)
-    yourimage.src = "images/icon-rock.svg";
-    switch(randomValue) {
-        case 0:
-            randomValue = "Rock";
-            imageadversary.src = "images/icon-rock.svg"
-            score.innerHTML = "Draw"
-            if (placarScore > 0) {
-                placarScore -= 1;
-            }
-            updateScoreboard()
-            break;
-        case 1:
-            randomValue = "Scissor";
-            imageadversary.src = "images/icon-scissors.svg"
-            score.innerHTML = "winner";
-            placarScore += 1;
-            updateScoreboard()
-            break;
-        case 2:
-            randomValue = "Paper";
-            imageadversary.src = "images/icon-paper.svg"
-            score.innerHTML = "Lost"
-            if (placarScore > 0) {
-                placarScore -= 1;
-            }
-            updateScoreboard()
-            break;
-    }
-});
+    const choices = ["rock", "scissor", "paper"];
+    const EnemyChoice = choices[randomValue];
 
-scissorbutton.addEventListener("click", function() {
-    function showLoading() {
-        loading.style.display = "block"
-        imageadversary.style.display = "none"
-    }
-    showLoading()
-    function hideloading() {
-        loading.style.display = "none"
-        imageadversary.style.display = "block"
-    }
-    let randomValue = Math.floor(Math.random() * 3);
-    yourimage.src = "images/icon-scissors.svg";
-    setTimeout(hideloading, 1000)
+    yourimage.src = `images/${playerChoice.toLowerCase()}.svg`;
+    imageadversary.src = `images/${EnemyChoice.toLowerCase()}.svg`;
 
-    switch(randomValue) {
-        case 0:
-            randomValue = "Rock";
-            imageadversary.src = "images/icon-rock.svg"
-            score.innerHTML = "Lost"
-             if (placarScore > 0) {
-                placarScore -= 1;
-            }
-            updateScoreboard()
-            break;
-        case 1:
-            randomValue = "Scissor";
-            imageadversary.src = "images/icon-scissors.svg"
-            score.innerHTML = "Draw"
-             if (placarScore > 0) {
-                placarScore -= 1;
-            }
-            updateScoreboard()
-            break;
-        case 2:
-            randomValue = "Paper";
-            imageadversary.src = "images/icon-paper.svg"
-            score.innerHTML = "winner"
-            placarScore += 1;
-            updateScoreboard()
-            break;
-    }
-});
+    if (playerChoice === EnemyChoice) {
+        updateScoreboard("drawn")
 
+        if (placarScore > 0) {
+            placarScore -= 1;
+        }
 
-paperbutton.addEventListener("click", function() {
-    function showLoading() {
-        loading.style.display = "block"
-        imageadversary.style.display = "none"
-    }
-    showLoading()
-    function hideloading() {
-        loading.style.display = "none"
-        imageadversary.style.display = "block"
-    }
-    yourimage.src = "images/icon-paper.svg";
-    
-    let randomValue = Math.floor(Math.random() * 3);
-    setTimeout(hideloading, 1000)
-    switch(randomValue) {
-        case 0:
-            randomValue = "Rock";
-            imageadversary.src = "images/icon-rock.svg"
-            score.innerHTML = "winner"
-            placarScore += 1;
-            updateScoreboard()
-            break;
-        case 1:
-            randomValue = "Scissor";
-            imageadversary.src = "images/icon-scissors.svg"
-            score.innerHTML = "Lost";
-             if (placarScore > 0) {
-                placarScore -= 1;
-            }
-            updateScoreboard()
-            break;
-        case 2:
-            randomValue = "Paper";
-            imageadversary.src = "images/icon-paper.svg"
-            score.innerHTML = "Draw"
-             if (placarScore > 0) {
-                placarScore -= 1;
-            }
-            updateScoreboard()
-            break;
-    }
-});
+    } else if (playerChoice === "rock" && EnemyChoice === "scissor" ||
+        playerChoice === "paper" && EnemyChoice === " rock" ||
+        playerChoice === "scissor" && EnemyChoice === "paper") {
+        updateScoreboard("winner")
 
-/*Rules */
+        placarScore += 1;
+    } else {
+        updateScoreboard("lost")
+        if (placarScore > 0) {
+            placarScore -= 1;
+        }
+    }
+}
 
-rules.addEventListener("click", function() {
+paperbutton.addEventListener("click", () => PlayGames("paper"))
+rockbutton.addEventListener("click", () => PlayGames("rock"))
+scissorbutton.addEventListener("click", () => PlayGames("scissor"))
+
+rules.addEventListener("click", function () {
     rulesimage.style.display = "block"
 })
